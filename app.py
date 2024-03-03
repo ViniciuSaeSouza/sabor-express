9c182f81d31401d0e8975cc91b087d02105cc339
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-02 21:20:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello World')
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolheu a opção', opcao_escolhida)
