8d1b5c606be13e5be46dbab33995c84d9a88cde5
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-04 14:13:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -9,9 +11,21 @@ print("""
       """) #aspas triplas geram uma nova linha, assim como "\n"
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('3. Ativar Restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def finalizar_app():
+      os.system('cls')
+      print('Finalizando App\n')
+
+if opcao_escolhida == 1:
+      print('Cadastrar Restaurante: ')
+elif opcao_escolhida == 2:
+      print('Listar Restaurantes')
+elif opcao_escolhida == 3:
+      print('Ativar Restaurante')
+else:
+      finalizar_app()
