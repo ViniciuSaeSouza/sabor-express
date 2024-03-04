e932caefda643ebbdd1e3d0a1c5ebd3bc29e972d
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-04 14:47:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
-print("""
-
+def exibir_nome_do_programa():
+      print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -10,22 +10,39 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """) #aspas triplas geram uma nova linha, assim como "\n"
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
+def exibir_opcoes():
+      print('1. Cadastrar restaurante')
+      print('2. Listar restaurantes')
+      print('3. Ativar Restaurante')
+      print('4. Sair\n')
 
 def finalizar_app():
       os.system('cls')
       print('Finalizando App\n')
 
-if opcao_escolhida == 1:
-      print('Cadastrar Restaurante: ')
-elif opcao_escolhida == 2:
-      print('Listar Restaurantes')
-elif opcao_escolhida == 3:
-      print('Ativar Restaurante')
-else:
-      finalizar_app()
+def escolher_opcao():
+      opcao_escolhida = int(input('Escolha uma opção: '))
+      match opcao_escolhida:
+            case 1:
+                  print('Cadastrar Restaurante: ')
+            case 2:
+                  print('Listar Restaurantes')
+            case 3:
+                  print('Ativar Restaurante')
+            case 4:
+                  finalizar_app()
+            case _:
+                  finalizar_app()
+                  print('Opção Inválida!')
+                  
+            
+      
+
+def main():
+      exibir_nome_do_programa()
+      exibir_opcoes()
+      escolher_opcao()
+      
+
+if __name__ == '__main__':
+      main()
