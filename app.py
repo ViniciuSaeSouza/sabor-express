901e0ac0a1dbca0a008589c926364721211f514a
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-10 12:43:58 -0300

    Aulas 3.5 e 3.7 + pratica

--- app.py
+++ app.py
@@ -19,30 +19,33 @@ def exibir_opcoes():
       print('4. Sair\n')
 
 def finalizar_app():
+      exibir_subtitulo('Finalizando App')
+      
+def exibir_subtitulo(texto):
       os.system('cls')
-      print('Finalizando App')
+      print(f'{texto}\n')
+      
+def voltar_ao_menu_principal():
+      input('\nDigite uma tecla para voltar ao menu principal: ')
+      main()
 
 def opcao_invalida():
       print('Opção Inválida\n')
-      input('Digite uma tecla para voltar ao menu principal')
-      main()
+      voltar_ao_menu_principal()
+      
             
 def cadastrar_novo_restaurante():
-      os.system('cls')
-      print('Cadastro de novos restaurantes\n')
+      exibir_subtitulo('Cadastro de novos restaurantes')
       nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
       restaurantes.append(nome_do_restaurante)
       print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-      input('\nDigite uma tecla para voltar ao menu principal')
-      main()
+      voltar_ao_menu_principal()
 
 def listar_restaurantes():
-      os.system('cls')
-      print('Restaurantes Listados:\n')
+      exibir_subtitulo('Restaurantes Listados:')
       for restaurante in restaurantes:
             print(f'-{restaurante}')     
-      input('\nDigite uma tecla para voltar ao menu principal')
-      main()
+      voltar_ao_menu_principal()
 
 def escolher_opcao():
       try:
